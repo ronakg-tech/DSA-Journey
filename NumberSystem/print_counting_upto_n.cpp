446921359f571a430788c1fe627d7a2e6395ce00
ronakg27 <guptaronak336@gmail.com>
2025-07-13 21:21:31 +0530

    Create print_counting_upto_n.cpp

---
+++ NumberSystem/print_counting_upto_n.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num;
+    cout << "enter an num: ";
+    cin >> num;
+
+    for(int i = 1; i <= num; i++){
+        cout << i << " ";
+
+    }
+    cout << endl;
+    return 0;
+}
