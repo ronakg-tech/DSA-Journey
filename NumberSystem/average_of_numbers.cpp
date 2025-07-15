35d9392460037321a9d6343f64b2eea0bd26a5da
ronakg27 <guptaronak336@gmail.com>
2025-07-15 08:00:05 +0530

    Create average_of_numbers.cpp

---
+++ NumberSystem/average_of_numbers.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a,b,c,d,e;
+    cout << "Enter Values: "<< endl;
+    cin >> a >> b >> c >> d >> e ;
+
+    float Avg = (a+b+c+d+e)/5;
+    cout << Avg;
+
+    return 0;
+
+
+}
